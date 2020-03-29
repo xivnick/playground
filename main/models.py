c9b0490c14015bba96e456244b3695e13333d61b
xivnick <xivnick@gmail.com>
2020-03-29 11:10:00 +0900

    Room 모델 생성 / 마이그레이션

--- main/models.py
+++ main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Room(models.Model):
+
+    STATE_CHOICES = (
+        ('W', 'waiting'),
+        ('I', 'in_game'),
+    )
+
+    GAME_CHOICES = (
+        ('CHAT', 'just_chat'),
+        ('MIGHTY', 'Mighty'),
+        ('NEMOS', 'Nemos'),
+    )
+
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    state = models.CharField(
+        max_length=1,
+        choices=STATE_CHOICES,
+        default='W'
+    )
+    game = models.CharField(
+        max_length=15,
+        choices=GAME_CHOICES,
+        default='CHAT'
+    )
+
+    def __str__(self):
+        return self.title
