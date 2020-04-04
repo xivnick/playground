e10d45c2bf026aa25a65083796df24e1d14e4941
xivnick <xivnick@gmail.com>
2020-04-04 20:29:40 +0900

    1.9버전부터 일어나는 static 파일 중복 에러 제거

--- playground/settings.py
+++ playground/settings.py
@@ -155,7 +155,7 @@ LOGIN_URL = '/account/login/'
 SASS_PROCESSOR_ROOT = STATIC_ROOT
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'sass_processor.finders.CssFinder',
 ]
 SASS_PRECISION = 8
